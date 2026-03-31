621da09316c31d5f27724d2d87affe1a88b4b118
Camilo Aviles <kmilo01077@yahoo.es>
2026-03-31 18:56:34 -0500

    Agrego carpeta con archivos

---
+++ LOGICA DE PROGRAMACIÓN DESDE CERO/reto00.py
@@ -0,0 +1,59 @@
+"""
+ * ¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
+ * - Recuerda que todas las instrucciones de participación están en el
+ *   repositorio de GitHub.
+ *
+ * Lo primero... ¿Ya has elegido un lenguaje?
+ * - No todos son iguales, pero sus fundamentos suelen ser comunes.
+ * - Este primer reto te servirá para familiarizarte con la forma de participar
+ *   enviando tus propias soluciones.
+ *
+ * EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ *
+ * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio.
+ """
+
+
+#https://www.python.org/. comentario de un renglon
+
+
+'''
+comentarios de más de un
+renglon
+'''
+"""
+comentarios se pueden con comillas
+simples o dobes como este caso
+"""
+
+#variable
+variable = 19
+#constante
+VARIABLE = 19
+
+#Diferentes formas de crear variables y tipos de variable
+#enteros
+variable_entero = 10
+variable_entero2: int = 10
+
+#flotantes
+variable_flotante = 15,0
+variable_flotante2: float = 15,0
+
+#boolean
+variable_boolean = False
+variable_boolean: bool = True
+
+#string
+variable_string = "Hola Mundo"
+
+print(variable_string)
