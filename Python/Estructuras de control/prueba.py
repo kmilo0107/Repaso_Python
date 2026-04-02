f81ee57e4e421f49d74515fbb03381eab8e9bf84
Camilo Aviles <kmilo01077@yahoo.es>
2026-04-01 20:12:22 -0500

    Se agrega archivo con nuevo tema

--- Python/Estructuras de control/prueba.py
+++ Python/Estructuras de control/prueba.py
@@ -1,11 +1,10 @@
+nombre = "KMILO"
+for i in nombre:
+    print(i)
+print("*************************")
 lista = ["A", "B", "C", "D"]
-indice = 0
-
-for x in lista:
-    print(f"Indice: {indice} valor: {x}")
-    indice += 1
-
-print("++++++++++++++++++++++++++++++++++++++++")
-
-for indice, x in enumerate(lista):
-    print(f"Indice: {indice:>10}| Valor: {x:>10}|")
+marca = iter(lista)
+print(next(marca))
+print(next(marca))
+print(next(marca))
+print(next(marca))
